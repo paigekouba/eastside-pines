8d0d76344a828e26ce685a0d64df58beb4ba54f2
paigekouba <pvkouba@ucdavis.edu>
2024-02-14 09:28:48 -0800

    histogram of gap size distributions with new patchmorph gaps; updated pie charts for each site in each year with patchmorph gaps and interstitial spaces

--- PatchMorph_ICO.R
+++ PatchMorph_ICO.R
@@ -178,7 +178,151 @@ opes1941 <- c(as.vector(opes_sr[[2]]$area, mode = "numeric"), as.vector(opes_sr[
 
 opes_all <- data.frame(Opes = c(opes1941, opes2018), Year = c(rep(1941, length(opes1941)), rep(2018, length(opes2018))))
 ggplot(opes_all, aes(x=Opes, fill=as.factor(Year))) +
-  geom_histogram(bins = 10, position="dodge") +
-  stat_bin(geom="text", bins=10, aes(label=after_stat(count), group=as.factor(Year)), position = position_dodge())+ 
-           scale_x_continuous(breaks = round(seq(57, 7779.12, length.out = 10)))
+  geom_histogram(bins = 11, position="dodge") +
+  stat_bin(geom="text", bins=11, aes(label=after_stat(count), group=as.factor(Year)), vjust = -0.5, position = position_dodge()) + 
+           scale_x_continuous(breaks = round(seq(50, 7550, length.out = 11))) +
+           scale_y_continuous(expand=expansion(mult=c(0,0.05))) +
+  labs(title = "Gap Size Distribution", hjust = 5, x = "Forest Canopy Gaps (m^2)", y = "Count", fill="Year") +
+  theme_classic()
 
+
+# Pie Charts -- IS (1941, 2018) and OH (1941, 2018)
+
+# pull area in clusters of each bin size
+# make a df with X, Y, crown, and bin, for plot # 9 as a test
+clust_area_df <- data.frame(X=plots_out[[9]]$trees$x, Y=plots_out[[9]]$trees$y, crown=plots_out[[9]]$trees$crown, bin=plots_out[[9]]$trees$bin)
+# split into bin levels, then calculate per-bin proportional areas
+#levels(plots_out[[9]]$trees$bin) # 6 levels
+# try this approach on cluster bin "2-4": make a polygon of just those trees, find the area of union, find intersection of that w bound
+two_four = st_buffer(st_as_sf(clust_area_df[clust_area_df$bin == "2-4",], coords = c("X","Y")), dist = clust_area_df[clust_area_df$bin == "2-4",]$crown) |> st_union() 
+plot(two_four)
+two_four_bound <- st_intersection(bound, st_cast(two_four))
+st_area(two_four_bound)*10000/st_area(bound_buff) # this finds the per-ha area of the shape representing all 2-4 clusters
+# this worked; need to do it over all bins, for all plots
+
+
+# make it a nested for lop: the outer loop applies, across all 12 plots, the inner loop to calculate proportional cluster area (across all bins)
+bin_names <- levels(plots_out[[9]]$trees$bin)
+
+# I now think this should use trees.noedge also
+
+clust_areas_all <- list()
+for (j in 1:length(plots_out)){
+  clust_area_df <- data.frame(X=plots_out[[j]]$trees.noedge$x, Y=plots_out[[j]]$trees.noedge$y, 
+                              crown=plots_out[[j]]$trees.noedge$crown, bin=plots_out[[j]]$trees.noedge$bin)
+  clust_areas <- list()
+  for (i in 1:length(bin_names)){ 
+    thingy <- clust_area_df %>% 
+      filter(bin==bin_names[i],) %>% 
+      st_as_sf(coords = c("X","Y")) %>% 
+      st_buffer(dist=clust_area_df$crown) %>% 
+      st_union() %>% 
+   #  st_cast("POLYGON") %>% 
+      st_intersection(bound) %>% 
+      st_area()
+    clust_areas[[i]] <- thingy
+  }
+  clust_areas_all[[j]] <- clust_areas 
+}
+library(rlist)
+piebins <- as.data.frame(list.rbind(clust_areas_all)) # make this a dataframe with one row per plot (12) and one column per clump size (6)
+piebins[piebins=="numeric(0)"] <- 0
+names(piebins) <- bin_names # change column names to reflect bin names
+piebins <- sapply(piebins, as.numeric) # need piebins not to be a list ! this fixes it
+piebins <- cbind(gap = 0, piebins) # add a column at left for gaps
+piebins <- cbind(interst = 0, piebins)
+piebins <- as.data.frame(piebins)
+# now need to draw total gap area from patchmorph section
+
+opes_perplot <- vector(mode = "numeric", length = 12)
+for (i in 1:length(plots_out)){
+  opes_perplot[i] <- sum(opes_sr[[i]]$area)
+}
+piebins$gap <- as.numeric(opes_perplot)
+piebins[4,6] # 0
+# lastly add interstitial space
+interst_perplot <- vector(mode = "numeric", length = 12)
+for (i in 1:length(plots_out)){
+  interst_perplot[i] <- 10000 - sum(unlist(piebins[i,]), na.rm=TRUE)
+}
+piebins$interst <- interst_perplot
+piebins <- piebins[,-8] # removes 30+ category since there were no observed clusters in that bin
+
+tpiebins <- as.data.frame(t(piebins))
+colnames(tpiebins) <- c(1:12)
+
+# ok we will try to make pie charts using piebins. Start with plot 9   
+library(RColorBrewer)
+my.cols <- brewer.pal(6, "YlGn")
+my.cols <- c("white", "#FEC44F", my.cols[2:6])
+
+library(scales)
+blank_theme <- theme_minimal()+
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid=element_blank(),
+    axis.ticks = element_blank(),
+    plot.title=element_text(size=14, face="bold")
+  )
+
+piefn <- function(plot) {
+  ggplot(tpiebins, aes(x="", y=tpiebins[,plot], fill=factor(rownames(tpiebins), levels=c(rownames(tpiebins))))) + #"interstitial", "gap",bin_names[1:5])))) +
+    geom_bar(width = 1, stat = "identity") +
+    coord_polar("y", start=0) +
+    # scale_fill_brewer(palette = "YlGn", name = "Cluster Size") 
+    scale_fill_manual(values = my.cols, name = "Cluster Size") + blank_theme +
+    theme(axis.text.x=element_blank()) +
+    labs(title = paste("Spatial Composition, ", names[plot]))
+}
+piefn(8) # works
+
+ICO_pies <- list()
+for (i in 1:length(plots_out)){
+  #jpeg(paste("ICO_Pie_",names[i]),700,630)
+  p <- piefn(i)
+  ICO_pies[[i]] <- p
+  #dev.off()
+}
+library(gridExtra)
+grid.arrange(ICO_pies[[1]], ICO_pies[[3]], ICO_pies[[5]], ICO_pies[[2]], ICO_pies[[4]], ICO_pies[[6]], ncol=3) #IS
+grid.arrange(ICO_pies[[7]], ICO_pies[[9]], ICO_pies[[11]], ICO_pies[[8]], ICO_pies[[10]], ICO_pies[[12]], ncol=3) #OH
+
+# one pie each for each site/year combo
+# first, get columns of tpiebins broken out: IS 2018 = 1, 3, 5; IS 1941 = 2, 4, 6     OH 2018 = 7, 9, 11; OH 1941 = 8, 10, 12
+bins_IS18 <- as.data.frame(sapply(rowSums(tpiebins[,c(1,3,5)]), as.numeric))
+bins_IS41 <- as.data.frame(sapply(rowSums(tpiebins[,c(2,4,6)]), as.numeric))
+bins_OH18 <- as.data.frame(sapply(rowSums(tpiebins[,c(7,9,11)]), as.numeric))
+bins_OH41 <- as.data.frame(sapply(rowSums(tpiebins[,c(8,10,12)]), as.numeric))
+
+# IS 2018
+pie_IS18 <- ggplot(bins_IS18, aes(x="", y=bins_IS18[,], fill=factor(rownames(bins_IS18), levels=c(rownames(bins_IS18))))) +
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0) +
+  scale_fill_manual(values = my.cols, name = "Cluster Size") + blank_theme +
+  labs(title = "Spatial Composition at IS, 2018")
+
+# IS 1941
+pie_IS41 <- ggplot(bins_IS41, aes(x="", y=bins_IS41[,], fill=factor(rownames(bins_IS41), levels=c(rownames(bins_IS41))))) +
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0) +
+  scale_fill_manual(values = my.cols, name = "Cluster Size") + blank_theme +
+  labs(title = "Spatial Composition at IS, 1941")
+
+# OH 2018
+pie_OH18 <- ggplot(bins_OH18, aes(x="", y=bins_OH18[,], fill=factor(rownames(bins_OH18), levels=c(rownames(bins_OH18))))) +
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0) +
+  scale_fill_manual(values = my.cols, name = "Cluster Size") + blank_theme +
+  labs(title = "Spatial Composition at OH, 2018")
+
+# OH 1941
+pie_OH41 <- ggplot(bins_OH41, aes(x="", y=bins_OH41[,], fill=factor(rownames(bins_OH41), levels=c(rownames(bins_OH41))))) +
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0) +
+  scale_fill_manual(values = my.cols, name = "Cluster Size") + blank_theme +
+  labs(title = "Spatial Composition at OH, 1941")
+
+grid.arrange(pie_IS41, pie_IS18, ncol=2)
+grid.arrange(pie_OH41, pie_OH18, ncol=2)
