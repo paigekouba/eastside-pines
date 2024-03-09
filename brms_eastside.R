d957b118b434739dfc0d941763c614e01ccc0533
paigekouba <pvkouba@ucdavis.edu>
2024-03-08 22:59:35 -0800

    re-run model with pre-fire years at each site

--- brms_eastside.R
+++ brms_eastside.R
@@ -12,6 +12,8 @@ for(i in 1:length(plots_out)) {
 library(rlist)
 MLR_df <- list.rbind(MLR_ICO)
 
+MLR_df[MLR_df$Year == 2015 | MLR_df$Year == 2006,]$Year <- "Prefire"
+
 MLR_df <- MLR_df %>% 
   group_by(Site, Plot.unique, Year, clust.sz) %>% 
   tally()
@@ -62,9 +64,12 @@ loo(pines1_nb,pines3_nb, pines4_nb, compare = TRUE) # pines3_nb has the best fit
     add_predicted_draws(pines3_nb) %>% # add best model here !!!
     ggplot(aes(x = clust.sz, y = n, color = Year, fill = Year)) +
     stat_lineribbon(aes(y = .prediction), .width = .95, alpha = 1/2) +
-    geom_point(data = MLR_df) +
-    scale_fill_brewer(palette = "Set2") +
-    scale_color_brewer(palette = "Dark2") +
+    #geom_point(data = MLR_df) +
+    geom_jitter(data = MLR_df, width = 0.2) +
+    scale_fill_manual(values = c("coral", "black", "darkgray")) +
+    scale_color_manual(values = c("red", "black", "darkgray")) +
+    #scale_fill_brewer(palette = "Set2") +
+    #scale_color_brewer(palette = "Dark2") +
     theme_bw() +
     ylab("\nFrequency") +
     xlab("\nTrees Per Cluster") +
@@ -77,25 +82,25 @@ summary(pines3_nb)
 # Family: negbinomial 
 # Links: mu = log; shape = identity 
 # Formula: n ~ clust.sz * Year + (1 | Plot.unique) 
-# Data: MLR_df (Number of observations: 66) 
+# Data: MLR_df (Number of observations: 67) 
 # Draws: 3 chains, each with iter = 3000; warmup = 1000; thin = 1;
 # total post-warmup draws = 6000
 # 
 # Group-Level Effects: 
 #   ~Plot.unique (Number of levels: 6) 
 # Estimate Est.Error l-95% CI u-95% CI Rhat Bulk_ESS Tail_ESS
-# sd(Intercept)     0.26      0.18     0.02     0.70 1.00     1578     2038
+# sd(Intercept)     0.22      0.17     0.01     0.67 1.00     1414     2278
 # 
 # Population-Level Effects: 
 #   Estimate Est.Error l-95% CI u-95% CI Rhat Bulk_ESS Tail_ESS
-# Intercept             4.51      0.28     3.95     5.06 1.00     3071     3298
-# clust.sz             -1.00      0.12    -1.23    -0.76 1.00     2967     3447
-# Year2018             -1.18      0.29    -1.75    -0.61 1.00     2880     3380
-# clust.sz:Year2018     0.55      0.12     0.30     0.79 1.00     2817     3318
+# Intercept             4.27      0.28     3.71     4.82 1.00     2495     3927
+# clust.sz             -0.89      0.12    -1.13    -0.64 1.00     2548     3377
+# Year2018             -0.98      0.30    -1.57    -0.40 1.00     2802     2989
+# clust.sz:Year2018     0.45      0.12     0.20     0.69 1.00     2593     3442
 # 
 # Family Specific Parameters: 
 #   Estimate Est.Error l-95% CI u-95% CI Rhat Bulk_ESS Tail_ESS
-# shape     7.20      3.96     2.98    16.82 1.00     3060     2419
+# shape     5.88      2.67     2.68    12.83 1.00     2921     2652
 # 
 # Draws were sampled using sampling(NUTS). For each parameter, Bulk_ESS
 # and Tail_ESS are effective sample size measures, and Rhat is the potential
