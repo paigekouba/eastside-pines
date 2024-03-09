458495749f29339c52319bc9a7fead41aa99ec43
paigekouba <pvkouba@ucdavis.edu>
2024-03-08 22:55:55 -0800

    update to include pre-fire years at each site

--- Ch1_figures.R
+++ Ch1_figures.R
@@ -1,19 +1,29 @@
 # Figures for Paper -- Eastside Pines ICO
 # Thu 2/15/24
 
-dotplots # percent change in nonspatial metrics, by site. 1941 in red, 2018 in black, with mean and se
+pdf(file = "Ch1_figures.pdf")
 
-ICO_maps # needs updating with PM gap areas, updated color scheme, and arrangement
+plot(dotplots) # percent change in nonspatial metrics, by site. 1941 in red, 2018 in black, with mean and se
 
-grid.arrange(pie_IS41, pie_IS18, ncol=2)
-grid.arrange(pie_OH41, pie_OH18, ncol=2) 
+# ICO_maps # needs updating with PM gap areas
+# two figs: IS 1, 2, 3 in a column for 1941 at left; IS 1, 2, 3 in a column for 2018 at right. Same for OH
+grid.arrange(ICO_maps[[2]], ICO_maps[[13]], ICO_maps[[1]], ICO_maps[[4]], ICO_maps[[14]], ICO_maps[[3]], ICO_maps[[6]], ICO_maps[[15]], ICO_maps[[5]], ncol=3) # IS
+grid.arrange(ICO_maps[[8]], ICO_maps[[16]], ICO_maps[[7]], ICO_maps[[10]], ICO_maps[[17]], ICO_maps[[9]], ICO_maps[[12]], ICO_maps[[18]], ICO_maps[[11]], ncol=3) # OH
+
+# grid.arrange(pie_IS41, pie_IS18, ncol=2)
+# grid.arrange(pie_OH41, pie_OH18, ncol=2) 
+grid.arrange(pie_IS41, pie_IS18, pie_OH41, pie_OH18, ncol=2)
 # before-and-after pie charts of all structural categories, by site
 
+# summ.all # per-bin and stand-level summary metrics for IS41, OH41, all41, IS18, OH18, and all18
 
-location_fit # brms model with cluster counts per cluster size
-model_coeffs # inset with model coefficients from brms model
+(gap_distn) # gap size histogram, combined
 
-gap_distn # gap size histogram, combined
+(location_fit) # brms model with cluster counts per cluster size
+# model_coeffs # inset with model coefficients from brms model
+caterplot(pines3nb_mcmc, parms = c("b_clust.sz", "b_Year2018", "b_clust.sz:Year2018"), pch=5, cex=1.5, lwd = c(4,8), labels = c("Cluster Size:Year","Cluster Size", "Year"), labels.loc="above", style = "plain")
+
+#Supplementary
 
 plot(OH_estab_cores) # establishment date distribution based on N = 81 cored trees
 plot(OH_estab_trees) # establishment date distribution based on N = 470 trees, using age-size regression
@@ -21,10 +31,16 @@ plot(IS_estab_cores) # establishment date distribution based on N = 90 cored tre
 plot(IS_estab_trees) # establishment date distribution based on N = 648 trees, using age-size regression
 
 # size class distn (dbh)
-IS_2018_hist
-IS_1941_hist
-OH_2018_hist
-OH_1941_hist
+plot(IS_2018_hist)
+plot(IS_1941_hist)
+plot(OH_2018_hist)
+plot(OH_1941_hist)
 
 model_v_cores_IS # compares regression-based ages with core-based ages
 model_v_cores_OH
+
+# math on area in interst, gap, size bins
+# IS_change
+# OH_change
+
+dev.off()
