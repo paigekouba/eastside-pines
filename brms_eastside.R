e1a34bef0c931a67afa1fa2ccada69819e3fb518
paigekouba <pvkouba@ucdavis.edu>
2024-03-19 23:42:46 -0700

    won't run, check packages?

--- brms_eastside.R
+++ brms_eastside.R
@@ -12,7 +12,8 @@ for(i in 1:length(plots_out)) {
 library(rlist)
 MLR_df <- list.rbind(MLR_ICO)
 
-MLR_df[MLR_df$Year == 2015 | MLR_df$Year == 2006,]$Year <- "Prefire"
+MLR_df[MLR_df$Year == 1995 | MLR_df$Year == 2006,]$Year <- "Prefire"
+#MLR_df[MLR_df$Year == 2018 | MLR_df$Year == 2009,]$Year <- "Postfire"
 
 MLR_df <- MLR_df %>% 
   group_by(Site, Plot.unique, Year, clust.sz) %>% 
@@ -50,6 +51,7 @@ pines4_nb <- brms::brm(n ~ clust.sz + Year + (1|Plot.unique),
 # There were 10 divergent transitions after warmup.
 
 loo(pines1_nb,pines3_nb, pines4_nb, compare = TRUE) # pines3_nb has the best fit
+AIC(pines1_nb,pines3_nb, pines4_nb)
 # Found 2 observations with a pareto_k > 0.7 in model 'pines3_nb'. It is recommended to set 'moment_match = TRUE' in order to perform moment matching for problematic observations.
 
 #loo(pines1_mbrms,pines3_mbrms, pines4_mbrms, compare = TRUE)
@@ -59,6 +61,7 @@ loo(pines1_nb,pines3_nb, pines4_nb, compare = TRUE) # pines3_nb has the best fit
 #performance::r2(pines3_nb)
 
 # try with just 95% CI
+library(tidyverse)
 (location_fit <- MLR_df %>%
     group_by(Year) %>%
     add_predicted_draws(pines3_nb) %>% # add best model here !!!
@@ -67,7 +70,7 @@ loo(pines1_nb,pines3_nb, pines4_nb, compare = TRUE) # pines3_nb has the best fit
     #geom_point(data = MLR_df) +
     geom_jitter(data = MLR_df, width = 0.2) +
     scale_fill_manual(values = c("coral", "black", "darkgray")) +
-    scale_color_manual(values = c("red", "black", "darkgray")) +
+    scale_color_manual(values = c("red", "black", "white")) +
     #scale_fill_brewer(palette = "Set2") +
     #scale_color_brewer(palette = "Dark2") +
     theme_bw() +
@@ -82,25 +85,27 @@ summary(pines3_nb)
 # Family: negbinomial 
 # Links: mu = log; shape = identity 
 # Formula: n ~ clust.sz * Year + (1 | Plot.unique) 
-# Data: MLR_df (Number of observations: 67) 
+# Data: MLR_df (Number of observations: 118) 
 # Draws: 3 chains, each with iter = 3000; warmup = 1000; thin = 1;
 # total post-warmup draws = 6000
 # 
 # Group-Level Effects: 
 #   ~Plot.unique (Number of levels: 6) 
 # Estimate Est.Error l-95% CI u-95% CI Rhat Bulk_ESS Tail_ESS
-# sd(Intercept)     0.22      0.17     0.01     0.67 1.00     1414     2278
+# sd(Intercept)     0.23      0.17     0.01     0.65 1.00     1312     2074
 # 
 # Population-Level Effects: 
 #   Estimate Est.Error l-95% CI u-95% CI Rhat Bulk_ESS Tail_ESS
-# Intercept             4.27      0.28     3.71     4.82 1.00     2495     3927
-# clust.sz             -0.89      0.12    -1.13    -0.64 1.00     2548     3377
-# Year2018             -0.98      0.30    -1.57    -0.40 1.00     2802     2989
-# clust.sz:Year2018     0.45      0.12     0.20     0.69 1.00     2593     3442
+# Intercept                4.25      0.30     3.66     4.82 1.00     1600     2709
+# clust.sz                -0.88      0.13    -1.14    -0.62 1.00     1659     2800
+# Year2018                -1.00      0.33    -1.65    -0.36 1.00     1999     2765
+# YearPrefire             -0.92      0.32    -1.54    -0.31 1.00     2061     3528
+# clust.sz:Year2018        0.46      0.14     0.19     0.73 1.00     1635     2820
+# clust.sz:YearPrefire     0.51      0.13     0.25     0.77 1.00     1702     2757
 # 
 # Family Specific Parameters: 
 #   Estimate Est.Error l-95% CI u-95% CI Rhat Bulk_ESS Tail_ESS
-# shape     5.88      2.67     2.68    12.83 1.00     2921     2652
+# shape     4.17      1.22     2.38     7.03 1.00     3804     3683
 # 
 # Draws were sampled using sampling(NUTS). For each parameter, Bulk_ESS
 # and Tail_ESS are effective sample size measures, and Rhat is the potential
@@ -108,5 +113,5 @@ summary(pines3_nb)
 
 library(mcmcplots)
 pines3nb_mcmc <- as.data.frame(as.matrix(as.mcmc(pines3_nb)))
-model_coeffs <- caterplot(pines3nb_mcmc, parms = c("b_clust.sz", "b_Year2018", "b_clust.sz:Year2018"), pch=5, cex=1.5, lwd = c(4,8), labels = c("Cluster Size:Year","Cluster Size", "Year"), labels.loc="above", style = "plain")
+model_coeffs <- caterplot(pines3nb_mcmc, parms = c("b_clust.sz", "b_YearPrefire", "b_clust.sz:YearPrefire"), pch=5, cex=1.5, lwd = c(4,8), labels = c("Cluster Size:Year","Year", "Cluster Size"), labels.loc="above", style = "plain")
 
