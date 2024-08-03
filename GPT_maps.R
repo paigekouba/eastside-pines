111c2e17d18f99abebc4ebdccd85cfa4a020a355
paigekouba <pvkouba@ucdavis.edu>
2024-08-02 22:28:32 -0700

    figure formatting

--- GPT_maps.R
+++ GPT_maps.R
@@ -14,7 +14,7 @@ for (i in 1:length(plots_out)){
           geom_sf(data=bound, fill="white") +
           geom_circle(data = as.data.frame(plots_out[[i]][11]), n=20, # #9 is trees; try 11 for trees.noedge
                       aes(x0 = trees.noedge.x, y0 = trees.noedge.y, r=trees.noedge.crown, x=trees.noedge.x, y=trees.noedge.y, 
-                          fill=factor(trees.noedge.bin), color=factor(trees.noedge.bin), alpha=0.75)) +
+                          fill=factor(trees.noedge.bin), color=factor(trees.noedge.bin)),alpha=0.75) +
           geom_circle(data = as.data.frame(plots_out[[i]][11]), n=20, 
        aes(x0=trees.noedge.x, y0=trees.noedge.y, r=trees.noedge.dbh/200), color="burlywood4", fill="burlywood4") +
         #  scale_fill_brewer(palette = "YlGn", name = "Cluster Size") +
@@ -26,12 +26,13 @@ for (i in 1:length(plots_out)){
           guides(size = guide_legend(override.aes = list(color ="burlywood4"))) +
           #theme_classic(base_size=22) +
           theme(plot.title=element_text(size = 10, hjust=0.5)) +
-          labs(title = paste("Living Trees at", names[i]),
+          labs( #title = paste("Living Trees at", names[i]),
                x = "",
                y = "",
                size = 10) +
                # size = "Crown (m)") +
-          theme(legend.position = "none")
+          theme_void() + theme(legend.text = element_text(size=18), legend.title = element_text(size=18)) #+ theme(legend.position = "none")
+   #theme(legend.position = "none")
           # guides(shape = guide_legend(override.aes = list(size = 1))) +
           # guides(color = guide_legend(override.aes = list(size = 1))) +
           # theme(legend.title = element_text(size = 5), legend.text = element_text(size = 5))
@@ -40,6 +41,8 @@ for (i in 1:length(plots_out)){
   # dev.off()
  ICO_maps[[i]] <- p
 }
+library(ggpubr)
+ggarrange(ICO_maps[[2]], ICO_maps[[13]], ICO_maps[[1]], ICO_maps[[4]], ICO_maps[[14]], ICO_maps[[3]], ICO_maps[[6]], ICO_maps[[15]], ICO_maps[[5]], ncol=3, nrow=3, common.legend = T, legend.grob = get_legend(ICO_maps[[13]]), legend = "right") # IS
 
 # double check the arrangement of the maps; remove per-map labels and add to grid.arrange ?
 # goal is two figs: IS 1, 2, 3 in a column for 1941 at left; IS 1, 2, 3 in a column for 2018 at right. Same for OH
